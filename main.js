f29594339e5565e06ab361ed4483e25cc695bce9
suresh328 <sureshloki325@gmail.com>
2024-09-29 15:44:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var bt =document.getElementById('tom');
+var list =document.getElementById('lim');
+
+bt.addEventListener('click',()=>{
+    
+    console.log('click');
+    list.classList.toggle('togle');
+})
+
+
